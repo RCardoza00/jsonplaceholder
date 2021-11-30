35845515f71692488dedcc37fbaa33db900f57f9
Ruben Cardoza <rubencardoza611@gmail.com>
2021-11-29 23:17:38 -0700

    jsonplaceholder api react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
 import './App.css';
+import { Component } from 'react';
+import axios from 'axios';
+class App extends Component {
+  state = {todos:[]};
+  async componentDidMount(){
+    let result = await axios.get("https://jsonplaceholder.typicode.com/todos")
+    this.setState({todos:result.data})
+    console.log(result)
+    console.log('testiris')
 
-function App() {
+  }
+ render(){
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+           <ul className="list-group">
+
+     {this.state.todos.length>0 ?<div>{this.state.todos.map((todo)=>
+     <li key={todo.id} className="list-group-item d-flex justify-content-between align-items-center">
+      {todo.title}
+       <span className="">
+         <input type="checkbox" checked={todo.completed}/>
+       </span>
+     </li>
+ 
+     )}</div>: <div className="spinner-border text-primary" role="status">
+   <span className="visually-hidden">Loading...</span>
+ </div>}
+ </ul>
     </div>
-  );
+ 
+  
+   );
+ }
 }
 
 export default App;
